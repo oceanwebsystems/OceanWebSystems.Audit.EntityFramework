465cacaa449dfe7a54d78ac33a965b2bfbf9e4f7
Stuart Whiteford <stuart@oceanwebsystems.com>
2022-01-11 14:05:33 +0000

    Add audit configuration options and attribute.

---
+++ OceanWebSystems.Audit.EntityFramework/AuditConfigurationAttribute.cs
@@ -0,0 +1,34 @@
+﻿namespace Audit.EntityFramework
+{
+    public class AuditConfigurationAttribute : Attribute
+    {
+        public AuditConfigurationAttribute(
+            string? tableNamePrefix = "",
+            string? tableNameSuffix = "AuditRecord",
+            string? softDeleteColumnName = "IsDeleted",
+            bool? softDeleteDeletedValue = true)
+        {
+            TableNamePrefix = tableNamePrefix;
+            TableNameSuffix = tableNameSuffix;
+            SoftDeleteColumnName = softDeleteColumnName;
+            SoftDeleteDeletedValue = softDeleteDeletedValue;
+        }
+
+        public AuditConfigurationAttribute(
+            AuditConfigurationOptions configOptions)
+            : this(configOptions.TableNamePrefix,
+                  configOptions.TableNameSuffix,
+                  configOptions.SoftDeleteColumnName,
+                  configOptions.SoftDeleteDeletedValue)
+        {
+        }
+
+        public string? TableNamePrefix { get; }
+
+        public string? TableNameSuffix { get; }
+
+        public string? SoftDeleteColumnName { get; }
+
+        public bool? SoftDeleteDeletedValue { get; }
+    }
+}
---
+++ OceanWebSystems.Audit.EntityFramework/AuditConfigurationOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Audit.EntityFramework
+{
+    public class AuditConfigurationOptions
+    {
+        public string? TableNamePrefix { get; set; }
+
+        public string? TableNameSuffix { get; set; }
+
+        public string? SoftDeleteColumnName { get; set; }
+
+        public bool? SoftDeleteDeletedValue { get; set; }
+    }
+}
